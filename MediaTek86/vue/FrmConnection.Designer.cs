dcba2601fb348c04208257d0c1bed9e9c875b2e6
Paul FENOUILLET <paul.terraria@gmail.com>
2023-05-31 13:17:37 +0200

    Création visuel de la vue

---
+++ MediaTek86/vue/FrmConnection.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace MediaTek86
+{
+    partial class FrmConnection
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(105, 45);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(177, 22);
+            this.txtLogin.TabIndex = 0;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(48, 45);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(51, 17);
+            this.lblLogin.TabIndex = 1;
+            this.lblLogin.Text = "Login :";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(105, 84);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(177, 22);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(22, 89);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(77, 17);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password :";
+            // 
+            // FrmConnection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.txtLogin);
+            this.Name = "FrmConnection";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+    }
+}
+
---
+++ MediaTek86/vue/FrmPersonnel.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace MediaTek86.vue
+{
+    partial class FrmPersonnel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstPersonnel = new System.Windows.Forms.ListBox();
+            this.btnAjouterPerso = new System.Windows.Forms.Button();
+            this.btnModifierPerso = new System.Windows.Forms.Button();
+            this.btnSupprimerPerso = new System.Windows.Forms.Button();
+            this.lstAbsence = new System.Windows.Forms.ListBox();
+            this.btnSupprimerAbs = new System.Windows.Forms.Button();
+            this.btnModifierAbs = new System.Windows.Forms.Button();
+            this.btnAjouterAbs = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAbsences = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstPersonnel
+            // 
+            this.lstPersonnel.FormattingEnabled = true;
+            this.lstPersonnel.ItemHeight = 16;
+            this.lstPersonnel.Location = new System.Drawing.Point(12, 48);
+            this.lstPersonnel.Name = "lstPersonnel";
+            this.lstPersonnel.Size = new System.Drawing.Size(246, 324);
+            this.lstPersonnel.TabIndex = 0;
+            // 
+            // btnAjouterPerso
+            // 
+            this.btnAjouterPerso.Location = new System.Drawing.Point(264, 48);
+            this.btnAjouterPerso.Name = "btnAjouterPerso";
+            this.btnAjouterPerso.Size = new System.Drawing.Size(122, 57);
+            this.btnAjouterPerso.TabIndex = 1;
+            this.btnAjouterPerso.Text = "Ajouter";
+            this.btnAjouterPerso.UseVisualStyleBackColor = true;
+            // 
+            // btnModifierPerso
+            // 
+            this.btnModifierPerso.Location = new System.Drawing.Point(264, 113);
+            this.btnModifierPerso.Name = "btnModifierPerso";
+            this.btnModifierPerso.Size = new System.Drawing.Size(122, 57);
+            this.btnModifierPerso.TabIndex = 2;
+            this.btnModifierPerso.Text = "Modifier";
+            this.btnModifierPerso.UseVisualStyleBackColor = true;
+            // 
+            // btnSupprimerPerso
+            // 
+            this.btnSupprimerPerso.Location = new System.Drawing.Point(264, 176);
+            this.btnSupprimerPerso.Name = "btnSupprimerPerso";
+            this.btnSupprimerPerso.Size = new System.Drawing.Size(122, 57);
+            this.btnSupprimerPerso.TabIndex = 3;
+            this.btnSupprimerPerso.Text = "Supprimer";
+            this.btnSupprimerPerso.UseVisualStyleBackColor = true;
+            // 
+            // lstAbsence
+            // 
+            this.lstAbsence.FormattingEnabled = true;
+            this.lstAbsence.ItemHeight = 16;
+            this.lstAbsence.Location = new System.Drawing.Point(392, 48);
+            this.lstAbsence.Name = "lstAbsence";
+            this.lstAbsence.Size = new System.Drawing.Size(267, 340);
+            this.lstAbsence.TabIndex = 4;
+            // 
+            // btnSupprimerAbs
+            // 
+            this.btnSupprimerAbs.Location = new System.Drawing.Point(665, 176);
+            this.btnSupprimerAbs.Name = "btnSupprimerAbs";
+            this.btnSupprimerAbs.Size = new System.Drawing.Size(122, 57);
+            this.btnSupprimerAbs.TabIndex = 5;
+            this.btnSupprimerAbs.Text = "Supprimer";
+            this.btnSupprimerAbs.UseVisualStyleBackColor = true;
+            // 
+            // btnModifierAbs
+            // 
+            this.btnModifierAbs.Location = new System.Drawing.Point(665, 113);
+            this.btnModifierAbs.Name = "btnModifierAbs";
+            this.btnModifierAbs.Size = new System.Drawing.Size(122, 57);
+            this.btnModifierAbs.TabIndex = 6;
+            this.btnModifierAbs.Text = "Modifier";
+            this.btnModifierAbs.UseVisualStyleBackColor = true;
+            // 
+            // btnAjouterAbs
+            // 
+            this.btnAjouterAbs.Location = new System.Drawing.Point(665, 50);
+            this.btnAjouterAbs.Name = "btnAjouterAbs";
+            this.btnAjouterAbs.Size = new System.Drawing.Size(122, 57);
+            this.btnAjouterAbs.TabIndex = 7;
+            this.btnAjouterAbs.Text = "Ajouter";
+            this.btnAjouterAbs.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Personnels :";
+            // 
+            // lblAbsences
+            // 
+            this.lblAbsences.AutoSize = true;
+            this.lblAbsences.Location = new System.Drawing.Point(392, 26);
+            this.lblAbsences.Name = "lblAbsences";
+            this.lblAbsences.Size = new System.Drawing.Size(172, 17);
+            this.lblAbsences.TabIndex = 9;
+            this.lblAbsences.Text = "Absences concernant --- :";
+            // 
+            // FrmPersonnel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAbsences);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAjouterAbs);
+            this.Controls.Add(this.btnModifierAbs);
+            this.Controls.Add(this.btnSupprimerAbs);
+            this.Controls.Add(this.lstAbsence);
+            this.Controls.Add(this.btnSupprimerPerso);
+            this.Controls.Add(this.btnModifierPerso);
+            this.Controls.Add(this.btnAjouterPerso);
+            this.Controls.Add(this.lstPersonnel);
+            this.Name = "FrmPersonnel";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstPersonnel;
+        private System.Windows.Forms.Button btnAjouterPerso;
+        private System.Windows.Forms.Button btnModifierPerso;
+        private System.Windows.Forms.Button btnSupprimerPerso;
+        private System.Windows.Forms.ListBox lstAbsence;
+        private System.Windows.Forms.Button btnSupprimerAbs;
+        private System.Windows.Forms.Button btnModifierAbs;
+        private System.Windows.Forms.Button btnAjouterAbs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblAbsences;
+    }
+}
